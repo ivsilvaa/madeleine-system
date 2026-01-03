1bd7534a33795f214459d4ba1765fb7fd831722b
Ivan Silva <ivdouglas.silva@gmail.com>
2026-01-02 23:54:15 -0300

    terminamos pagiona de cadastros vazia e adicionamos responsividade a ela em todas as versoes de tela

--- app/(dashboard)/cadastros/sem_cadastros/page.tsx
+++ app/(dashboard)/cadastros/sem_cadastros/page.tsx
@@ -1,24 +1,57 @@
 export default function Page() {
     return (
-        <section>
-            <div className="mt-6 flex justify-center items-center gap-3">
-                {/* Campo de pesquisa fake */}
+        <section className="w-full overflow-x-hidden">
+
+            {/* Barra de pesquisa */}
+            <div className="mt-6 flex justify-center items-center gap-3 px-4 w-full max-w-6xl mx-auto">
+
                 <input
                     type="text"
                     placeholder="Pesquisar"
-                    className="flex items-center justify-start bg-white w-212 h-12 rounded-2xl px-4 outline-none
-                    border border-gray-300 focus:border-gray-400">
-                </input>
+                    className="
+                        bg-white h-12 rounded-2xl px-4 outline-none
+                        border border-gray-300 focus:border-gray-400
+                        flex-1
+                    "
+                />
 
-                {/* Botão */}
                 <button
                     type="submit"
-                    className="flex items-center justify-center bg-black text-white w-50 h-12 rounded-2xl cursor-pointer"
+                    className="
+                        flex items-center justify-center
+                        bg-black text-white h-12 rounded-2xl cursor-pointer
+                        w-32
+                    "
                 >
                     <p className="text-base font-semibold">Adicionar</p>
                 </button>
-            </div>
-        </section>
 
-    )
+            </div>
+
+            {/* Dashboard */}
+            <div
+                className="
+                    bg-white border border-gray-300 rounded-2xl
+                    mt-6 px-4
+                    flex flex-col gap-4 items-center justify-center
+
+                    w-full max-w-6xl mx-auto
+                    h-[60vh] sm:h-[65vh] lg:h-[70vh]
+
+                    overflow-hidden
+                "
+            >
+                <h2 className="text-gray-300 text-2xl text-center">
+                    Nenhum valor encontrado
+                </h2>
+
+                <img
+                    src="/icon-no_result.png"
+                    alt="Ícone de sem resultado"
+                    className="w-76 h-52 object-contain opacity-20"
+                />
+            </div>
+
+        </section>
+    );
 }
